3b61c3caf7bccd04394a7b53bd91613ef7d1a88b
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-04 16:12:32 +0100

    Navbar on top of page.

--- header.php
+++ header.php
@@ -24,7 +24,46 @@
 <body <?php body_class(); ?>>
 	<?php do_action( 'before' ); ?>
 
-<header id="masthead" class="site-header" role="banner">
+	<header class="site-navigation">
+	<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+		<div class="container">
+			<div class="row">
+				<div class="site-navigation-inner col-sm-12">
+					<div class="navbar navbar-default">
+						<div class="navbar-header">
+							<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only"><?php _e('Toggle navigation','verapdf') ?> </span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+
+							<!-- Your site title as branding in the menu -->
+							<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						</div>
+
+						<!-- The WordPress Menu goes here -->
+						<?php wp_nav_menu(
+							array(
+								'theme_location' 	=> 'primary',
+								'depth'             => 2,
+								'container'         => 'div',
+								'container_class'   => 'collapse navbar-collapse',
+								'menu_class' 		=> 'nav navbar-nav',
+								'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
+								'menu_id'			=> 'main-menu',
+								'walker' 			=> new wp_bootstrap_navwalker()
+							)
+						); ?>
+
+					</div><!-- .navbar -->
+				</div>
+			</div>
+		</div><!-- .container -->
+	</header><!-- .site-navigation -->
+
+<div id="masthead" class="site-header" role="banner">
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
 	<div class="container">
 		<div class="row">
@@ -46,50 +85,10 @@
 			</div>
 		</div>
 	</div><!-- .container -->
-</header><!-- #masthead -->
-
-<nav class="site-navigation">
-<?php // substitute the class "container-fluid" below if you want a wider content area ?>
-	<div class="container">
-		<div class="row">
-			<div class="site-navigation-inner col-sm-12">
-				<div class="navbar navbar-default">
-					<div class="navbar-header">
-						<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-							<span class="sr-only"><?php _e('Toggle navigation','verapdf') ?> </span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-	
-						<!-- Your site title as branding in the menu -->
-						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</div>
-
-					<!-- The WordPress Menu goes here -->
-					<?php wp_nav_menu(
-						array(
-							'theme_location' 	=> 'primary',
-							'depth'             => 2,
-							'container'         => 'div',
-							'container_class'   => 'collapse navbar-collapse',
-							'menu_class' 		=> 'nav navbar-nav',
-							'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
-							'menu_id'			=> 'main-menu',
-							'walker' 			=> new wp_bootstrap_navwalker()
-						)
-					); ?>
-
-				</div><!-- .navbar -->
-			</div>
-		</div>
-	</div><!-- .container -->
-</nav><!-- .site-navigation -->
+</div><!-- #masthead -->
 
 <div class="main-content">
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
 	<div class="container">
 		<div class="row">
 			<div id="content" class="main-content-inner col-sm-12 col-md-8">
-
