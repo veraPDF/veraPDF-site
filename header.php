14184a7a20c469598564124b26696256736f13b9
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-05 13:02:38 +0100

    Improved HTML <head>:  * added metadata;  * html5shiv & respond for IE8 or less.

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="The veraPDF project, definitive, open source PDF/A validation.">
+	<meta name="author" content="veraPDF Consortium">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
@@ -19,6 +21,12 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
+
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
