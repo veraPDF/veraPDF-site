b644cd0a0a42eb69611be6f7f3a17eb15a68159e
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-10 03:43:25 +0100

    Removed cover page menu.

--- header-cover.php
+++ header-cover.php
@@ -9,17 +9,3 @@
 ?><div class="site-wrapper">
   <div class="site-wrapper-inner">
     <div class="cover-container">
-
-      <div class="masthead clearfix">
-        <div class="inner">
-          <h3 class="masthead-brand">veraPDF</h3>
-          <nav>
-            <ul class="nav masthead-nav">
-              <li><a href="<?php echo get_permalink( get_page_by_path( 'verapdf' ) ) . '/#validation'; ?>">PDF/A Validation</a></li>
-              <li><a href="<?php echo get_permalink( get_page_by_path( 'verapdf' ) ) . '/#licensing'; ?>">Licensing</a></li>
-              <li><a href="<?php echo get_permalink( get_page_by_path( 'verapdf' ) ) . '/#about'; ?>">About</a></li>
-              <li><a href="<?php echo get_permalink( get_page_by_path( 'contact' ) ); ?>">Contact</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
