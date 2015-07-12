7b2aa598563fe74f4bec114cce9cb72b2a32003a
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-13 00:18:33 +0100

    Styling tweaks.

--- registration-form.php
+++ registration-form.php
@@ -69,7 +69,7 @@ get_header(); ?>
     		<div class="response" id="mce-success-response" style="display:none"></div>
     	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
       <div style="position: absolute; left: -5000px;"><input type="text" name="b_c2ba434b13896e6e89790a724_64ff32b55b" tabindex="-1" value=""></div>
-      <div class="clear text-center"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-lg btn-danger"></div>
+      <div class="clear text-center"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-lg btn-primary"></div>
     </form>
   </div>
 </div>
