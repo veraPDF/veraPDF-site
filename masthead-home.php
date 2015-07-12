c67f0d0078bbecf6cd9a735e90517a72483e8c0e
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-12 16:44:33 +0100

    Alt and title now on logo.

--- masthead-home.php
+++ masthead-home.php
@@ -16,8 +16,8 @@
 							<p class="lead">
 							<?php $header_image = get_header_image();
 							if ( ! empty( $header_image ) ) { ?>
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"  alt="The veraPDF logo" title="veraPDF logo">
-									<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+									<img src="<?php header_image(); ?>" alt="The veraPDF logo" title="veraPDF logo" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>">
 								</a>
 							<?php } // end if ( ! empty( $header_image ) ) ?>
 						  </p>
