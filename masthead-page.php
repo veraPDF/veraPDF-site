c86e185f3ef1418ad466b0dc94631ceb2b57927e
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-12 21:29:58 +0100

    Fixed page header radial mask.

--- masthead-page.php
+++ masthead-page.php
@@ -7,16 +7,12 @@
  * @package Verapdf
  */
 ?>
-<header id="masthead" class="page-header" role="banner">
+<header id="masthead" class="page-header radialmask" role="banner">
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
-  <div class="radialmask">
     <div class="container">
       <div class="row">
-        <div class="site-header-inner col-sm-12">
           <h1><?php echo $wp_query->post->post_title; ?></h1>
           <p class="lead"><?php echo get_post_meta($wp_query->post->ID, 'tag-line', true); ?></p>
-        </div>
       </div><!-- .row -->
     </div><!-- .container -->
-  </div><!-- .mask -->
 </header><!-- #masthead -->
