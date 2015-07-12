cc1539d5b7795fbdf9cdcfcd10ae5f9ac2136ea6
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-13 00:52:10 +0100

    Cover text aligned with buttons.

--- cover-page.php
+++ cover-page.php
@@ -9,17 +9,24 @@
 get_header(); ?>
           <div class="inner cover">
             <header id="masthead">
-              <h1><?php echo get_post_meta(get_option('page_on_front'), 'tag-line', true); ?></h1>
-              <p class="lead">
-                <?php $header_image = get_header_image();
-                if ( ! empty( $header_image ) ) { ?>
-                  <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="The veraPDF logo" title="veraPDF logo">
-                <?php } // end if ( ! empty( $header_image ) ) ?>
-              </p>
-              <?php
-              $post = get_post(get_option('page_on_front'));
-              echo $post->post_content;
-              ?>
+              <div class="row">
+                <h1><?php echo get_post_meta(get_option('page_on_front'), 'tag-line', true); ?></h1>
+                <p class="lead">
+                  <?php $header_image = get_header_image();
+                  if ( ! empty( $header_image ) ) { ?>
+                    <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="The veraPDF logo" title="veraPDF logo">
+                  <?php } // end if ( ! empty( $header_image ) ) ?>
+                </p>
+              </div>
+              <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                  <?php
+                  $post = get_post(get_option('page_on_front'));
+                  echo $post->post_content;
+                  ?>
+                </div>
+              </div>
             </header>
             <nav class="container">
               <div class="row">
