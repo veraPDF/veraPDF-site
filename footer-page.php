3c06c24800ce4291cd388f694befd931ee6d2599
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-13 19:14:01 +0100

    Added CC license to footer.

--- footer-page.php
+++ footer-page.php
@@ -16,13 +16,19 @@
   <?php // substitute the class "container-fluid" below if you want a wider content area ?>
   <div class="container">
     <div class="row">
-      <div class="site-footer-inner col-sm-12">
-
+      <div class="site-footer-inner col-sm-2">
+      </div>
+      <div class="site-footer-inner col-sm-8">
         <div class="site-info">
           <?php do_action( 'verapdf_credits' ); ?>
           <span><?php _e('Copyright &copy; 2015 veraPDF consortium','verapdf') ?></span>
         </div><!-- close .site-info -->
-
+      </div>
+      <div class="site-footer-inner col-sm-2">
+        <div class="cc-info text-left small">
+          <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />
+          <small>Except where otherwise noted site content created by the <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.verapdf.org" property="cc:attributionName" rel="cc:attributionURL"> veraPDF Consortium</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small>
+        </div>
       </div>
     </div>
   </div><!-- close .container -->
