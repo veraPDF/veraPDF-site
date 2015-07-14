c6d46c585fd7d38298adc3afb4fd05da6b128baa
Carl Wilson <carl@openplanetsfoundation.org>
2015-07-14 14:01:34 +0100

    Added favicon.

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
+  <link href="<?php echo get_template_directory_uri() . '/includes/img/favicon.ico'; ?>" rel="icon" type="image/x-icon" />
 
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
