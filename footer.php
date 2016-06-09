6f4804cc505e18f9f07e8b068f7a563ecbb67887
Carl Wilson <carl@openplanetsfoundation.org>
2016-06-09 15:51:25 +0100

    FEAT - Remove cover page - veraPDF menu item renamed -> Home; - removed cover-page.php and css; and - removed dedicate cover header and footer;

--- footer.php
+++ footer.php
@@ -6,21 +6,49 @@
  *
  * @package Verapdf
  */
-get_template_part('footer', is_front_page() ? 'cover' : 'page'); ?>
+?>
+      </div><!-- close .*-inner (main-content or sidebar, depending if sidebar is used) -->
+    </div><!-- close .row -->
+  </div><!-- close .container -->
+</section><!-- close .main-content -->
 
+<footer id="colophon" class="site-footer" role="contentinfo">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+  <div class="container">
+    <div class="row">
+     <div class="site-footer-inner col-sm-2">
+       <div class="back-to-top text-left">
+         <a class="back-to-top" href="#top">Back to top</a>
+       </div>
+     </div>
+     <div class="site-footer-inner col-sm-8">
+       <div class="site-info">
+         <?php do_action( 'verapdf_credits' ); ?>
+         <span><?php _e('Copyright &copy; 2015 veraPDF consortium','verapdf') ?></span>
+       </div><!-- close .site-info -->
+     </div>
+     <div class="site-footer-inner col-sm-2">
+       <div class="cc-info text-left small">
+         <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />
+         <small>Except where otherwise noted site content created by the <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.verapdf.org" property="cc:attributionName" rel="cc:attributionURL"> veraPDF Consortium</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</small>
+       </div>
+     </div>
+  </div>
+  </div><!-- close .container -->
+</footer><!-- close #colophon -->
 <?php wp_footer(); ?>
 
 </body>
 </html>
 <svg height="0" width="0" xmlns="http://www.w3.org/2000/svg">
-<filter id="drop-shadow">
-<feGaussianBlur in="SourceAlpha" stdDeviation="2.2"/>
-<feOffset dx="12" dy="12" result="offsetblur"/>
-<feFlood flood-color="rgba(0,0,0,0.5)"/>
-<feComposite in2="offsetblur" operator="in"/>
-<feMerge>
-<feMergeNode/>
-<feMergeNode in="SourceGraphic"/>
-</feMerge>
-</filter>
+  <filter id="drop-shadow">
+    <feGaussianBlur in="SourceAlpha" stdDeviation="2.2"/>
+    <feOffset dx="12" dy="12" result="offsetblur"/>
+    <feFlood flood-color="rgba(0,0,0,0.5)"/>
+    <feComposite in2="offsetblur" operator="in"/>
+    <feMerge>
+      <feMergeNode/>
+      <feMergeNode in="SourceGraphic"/>
+    </feMerge>
+  </filter>
 </svg>
